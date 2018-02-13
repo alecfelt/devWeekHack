087eba1a24099d911a2c3b2391f903c8b92ddbfd
Alex <amosrey@localhost.localdomain>
2018-02-12 21:38:07 -0800

    Added server

---
+++ EfficiencyTracker/server/server.js
@@ -0,0 +1,21 @@
+// server.js
+
+// defining express
+const express = require('express');
+const app = express();
+
+// running server on port 3000
+app.listen(3000, function() {
+  console.log('listening on 3000')
+})
+
+// get request
+app.get('/', (req, res) => {
+  res.send('You are connected')
+})
+
+// post request
+app.post('/', (req, res) => {
+  console.log('Someone just posted')
+  res.send('You just posted')
+})
